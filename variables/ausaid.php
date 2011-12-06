25b0e1aaefbe02f56f18b1fa90a6eb7c0358cf73
caprenter <caprenter@gmail.com>
2011-12-06 11:21:07 +0000

    Temporary Config change for AusAID variables
    - Sets the url to the test server for validation purposes

--- variables/ausaid.php
+++ variables/ausaid.php
@@ -1,4 +1,5 @@
 <?php
 $dir = 'data/ausaid/'; //needs trailing slash
-$url = 'http://www.ausaid.gov.au/about/';
+//$url = 'http://www.ausaid.gov.au/about/';
+$url = 'http://bntest.vm.bytemark.co.uk/david/iati/batch/data/ausaid/';
 ?>
